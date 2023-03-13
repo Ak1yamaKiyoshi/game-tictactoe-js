997dccec93b7d86469974cd103a91d7a8e90fc0b
Ak1yamaKiyoshi <avoqun@gmail.com>
2023-03-13 15:08:37 +0200

    1

--- Multiplayer/index.js
+++ Multiplayer/index.js
@@ -4,6 +4,8 @@ class Player {
         this.nowPlaying = "X" // X or O 
     }
 
+    // 0 0 0 0  0 0 0 0
+    // 0 0 0 0  0 0 0 1
     move(i) {
         const moves = [0x1, 0x2, 0x4, 0x8, 0x10, 0x20, 0x40, 0x80, 0x100, 0x200];
         this.score |= moves[i];  
---
+++ Multiplayer/server.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const express = require('express')
+const WebSocket = require('ws');
+
+const port = 4000;
+const server = http.createServer(express)
+const wss = new WebSocket.Server( { server} );
+
+wss.on('connection', function connection(ws) {
+    ws.on('message', function incoming(data) {
+        wss.clients.forEach(function each(client) {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(data);
+            }
+        })
+    });
+})
+server.listen(port, function() {
+    console.log('server is listening on port: ', port);
+})
